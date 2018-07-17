0dd20d5d234469cb7af51ef672bf88e802981b37
Marina Valiquette <marina.valiquette@gmail.com>
2018-07-17 10:32:58 -0400

    Add prop-type dep. Add field amd registerForm components

---
+++ src/actions/Forms/field.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Field = props => {
+  console.log('Field component rendered');
+  const Element = props.element || 'input';
+  return (
+    <div className="form-field" id={props.id}>
+      <label htmlFor={props.input.name}>{props.label}
+        <Element
+          // id={props.input.name}
+          className={(props.meta.error && props.meta.touched) ? 'error-border' : 'placeholder-class'}
+          type={props.type}
+          value={props.value}
+          aria-required="true"
+          // placeholder={props.label}
+          {...props.input}
+        >
+          {props.children}
+        </Element>
+      </label>
+      {/*
+        If error evaluates to true -> show span element
+        if error is undefined -> React ignores the span element
+      */}
+      {props.meta.error &&
+      props.meta.touched &&
+      <div className="form-error" aria-live="polite" role="alert">{props.meta.error}</div>}
+    </div>
+    // </React.Fragment>
+  );
+};
+
+export default Field;
---
+++ src/actions/Forms/registerForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class RegisterForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      page: 1,
+    }
+  }
+
+
+
+  render() {
+    console.log('PROPS', this.props);
+  }
+}
+
+
