9079d9600630daf13327c20e03b15642bedcd807
Marina Valiquette <marina.valiquette@gmail.com>
2018-07-16 21:27:40 -0400

    Add reducers dir, root reducer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
+import rootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { loadAuthToken } from './local-storage';
-import { setAuthToken, refreshAuthToken } from './actions/auth';
+// import { loadAuthToken } from './local-storage';
+// import { setAuthToken, refreshAuthToken } from './actions/auth';
 
 const store = createStore(
   rootReducer,
@@ -13,11 +13,11 @@ const store = createStore(
 );
 
 // Hydrate the authToken from localStorage if it exist
-const authToken = loadAuthToken();
-if (authToken) {
-  const token = authToken;
-  store.dispatch(setAuthToken(token));
-  store.dispatch(refreshAuthToken());
-}
+// const authToken = loadAuthToken();
+// if (authToken) {
+//   const token = authToken;
+//   store.dispatch(setAuthToken(token));
+//   store.dispatch(refreshAuthToken());
+// }
 
 export default store;
