af172a046dc5201a579dd3e0777a67d70376cc0b
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-18 09:47:16 -0600

    Add reducers for frame actions

---
+++ src/reducers/frames.js
@@ -0,0 +1,35 @@
+import { REQUEST_FRAMES, FRAMES_SUCCESS, FRAMES_ERROR } from '../actions/frames';
+
+const initialState = {
+  frames: [],
+  loading: false,
+  error: null
+};
+
+export default function framesReducer(state = initialState, action){
+  if (action.type === REQUEST_FRAMES){
+    return {
+      ...state,
+      loading: true
+    };
+  }
+  
+  if (action.type === FRAMES_SUCCESS){
+    return {
+      ...state,
+      loading: false,
+      frames : action.data,
+      error: null
+    };
+  }
+
+  if (action.type === FRAMES_ERROR){
+    return {
+      ...state,
+      loading: false,
+      error: action.error
+    };
+  }
+
+  return state;
+}
