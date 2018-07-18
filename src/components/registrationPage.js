203a41208ff574f66d352404eb44570b759112cc
patersog <gianluca.paterson@gmail.com>
2018-07-18 12:26:57 -0400

    Fixed unresolved merge

--- src/components/registrationPage.js
+++ src/components/registrationPage.js
@@ -1,47 +1,35 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
-import {connect} from "react-redux";
+import {connect} from 'react-redux';
+
 import NavBar from './navBar';
 import RegisterForm from './Forms/registerForm';
 import Footer from './footer';
 
-<<<<<<< HEAD
-export default function RegistrationPage() {
-	return (
-		<main>
-			<NavBar />
-			<section>
-				<RegisterForm />
-			</section>
-			<Footer />
-		</main>
-	);
-}
-=======
 
 function RegistrationPage(props) {
-  if(props.loggedIn) {
-    return <Redirect to='/dashboard' />
-  }
+	if(props.loggedIn) {
+		return <Redirect to='/dashboard' />;
+	}
 
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main>
-        <section>
-          <RegisterForm />
-        </section>
-      </main>
-      <Footer />
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<NavBar />
+			<main>
+				<section>
+					<RegisterForm />
+				</section>
+			</main>
+			<Footer />
+		</React.Fragment>
+	);
 }
 
+
 const mapStateToProps = state => {
-  return {
-    loggedIn : state.auth.authToken !== null
-  };
+	return {
+		loggedIn : state.auth.authToken !== null
+	};
 };
 
 export default connect(mapStateToProps)(RegistrationPage);
->>>>>>> af172a046dc5201a579dd3e0777a67d70376cc0b
