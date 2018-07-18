6345f72b3c07b090cf3764af35f55fc39294247f
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-18 14:38:07 -0600

    Add company profile editing capability

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,24 +1,88 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { fetchProfile } from '../actions/profile';
+import InnerNav from './inner-nav';
+import Footer from './footer';
 
 export class Profile extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      editing : false
+    };
+  }
+
   componentWillMount(){
-    console.log('fetching profile');
     this.props.dispatch(fetchProfile(this.props.adminId));
   }
 
+  handleEdit(){
+    this.setState({editing : true});
+  }
+
+  handleCancel(){
+    this.setState({editing: false});
+  }
+
   render(){
-    return (<div>
-      test
-      </div>);
+
+    if (this.state.editing){
+      return ( <main>
+        <InnerNav />
+        <header>
+          <h1>{this.props.name}</h1>
+        </header>
+        <section>
+        </section>
+        <button
+          onClick={() => this.handleCancel()}
+        >
+          Cancel
+        </button>
+      </main>
+      );
+    }
+    return (
+      <main>
+        <InnerNav />
+        <header>
+          <h1>{this.props.name}</h1>
+        </header>
+        <section>
+          <ul>
+            <li>
+              <h2>Username</h2>
+              <p>{this.props.username}</p>
+            </li>
+            <li>
+              <h2>Email Address</h2>
+              <p>{this.props.email}</p>
+            </li>
+            <li>
+              <h2>Phone Number</h2>
+              <p>{this.props.phone}</p>
+            </li>
+          </ul>
+          <button
+            onClick={() => this.handleEdit()}
+          >
+            Edit
+          </button>
+        </section>
+        <Footer />
+      </main>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    adminId : state.auth.user.id
+    adminId : state.auth.user.id,
+    username : state.profile.data.username,
+    name : state.profile.data.companyName,
+    phone : state.profile.data.phoneNumber,
+    email: state.profile.data.email
   };
 };
 
