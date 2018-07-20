d8c151300ebfdbbc3c3acb21b18951cf283a20cf
Cam Prier <CSPrier@gmail.com>
2018-07-20 12:08:25 -0700

    created reassign shift actions and async fetch function

---
+++ src/actions/reassignShift.js
@@ -0,0 +1,42 @@
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from './utils';
+
+export const REASSIGN_SHIFT = 'REASSIGN_SHIFT';
+export const reassignShift = () => {
+	return {
+		type: REASSIGN_SHIFT
+	};
+};
+
+export const REASSIGN_SUCCESS = 'REASSIGN_SUCCESS';
+export const reassignSuccess = data => {
+	return {
+		type: REASSIGN_SUCCESS,
+		data
+	};
+};
+
+export const REASSIGN_ERROR = 'REASSIGN_ERROR';
+export const reassignError = error => {
+	return {
+		type: REASSIGN_ERROR,
+		error
+	};
+};
+
+export const fetchReassignShift = (data) => dispatch => {
+	const token = localStorage.getItem('authToken');
+	dispatch(reassignShift());
+	return fetch(`${API_BASE_URL}/frames/frames/${data.frameId}`, {
+		method: 'PUT',
+		headers : {
+			'Content-Type' : 'application/json',
+			'Authorization' : `Bearer ${token}`
+		},
+		body: JSON.stringify(data)
+	})
+		.then(res => normalizeResponseErrors(res))
+		.then(res => res.json())
+		.then(data => dispatch(reassignSuccess(data)))
+		.catch(error => dispatch(reassignError(error.message)));
+};
