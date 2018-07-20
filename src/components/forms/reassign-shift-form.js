3fcf55352551e23cbadd8dea2bf8a1863f377f60
Cam Prier <CSPrier@gmail.com>
2018-07-20 15:25:13 -0700

    Attempting to fix reassign functonality

--- src/components/forms/reassign-shift-form.js
+++ src/components/forms/reassign-shift-form.js
@@ -3,9 +3,9 @@ import {connect} from 'react-redux';
 
 import './styles/forms.css';
 import { fetchReassignShift } from '../../actions/reassignShift';
+import { fetchFrames } from '../../actions/frames';
 
 export class ReassignShiftForm extends React.Component {
-
 	constructor(props) {
 		super(props);
 
@@ -13,33 +13,43 @@ export class ReassignShiftForm extends React.Component {
 
 	componentDidMount() {
 		/* Make the call to the API to fetch employees */
-		//this.props.dispatch();
+		// this.props.dispatch(fetchFrames());
 	}
 
-	handleSubmit(e) {
+	handleSubmit(e, values) {
 		e.preventDefault();
-		console.log(e.value);
+		const data = new FormData(e.target);
+		// console.log(`data: ${data}`);
+		console.log(values);
 		// this.props.dispatch(fetchReassignShift(e.data));
 	}
 
 	render() {
+		// console.log('FRAMES:', this.props.frames.frames.map(frame => <option value={`${frame.employeeId.firstname} ${frame.employeeId.lastname}`}>
+		// 	{`${frame.employeeId.firstname} ${frame.employeeId.lastname}`}
+		// </option>));
 		return(
 			<div className="form-wrapper">
 				<h2 className="form-header">Reassign Shift</h2>
-				<form onSubmit={this.handleSubmit}>
+				<form onSubmit={(e) => this.handleSubmit(e)}>
 					<select>
-						<option value='Employee1'>Employee1</option>
-						<option value='Employee1'>Employee2</option>
-						<option value='OPEN'>OPEN</option>
+						<option>SELECT EMPLOYEE</option>
+						{this.props.frames.frames.map(frame => {
+							<option value={`${frame.employeeId.firstname} ${frame.employeeId.lastname}`}>
+								{`${frame.employeeId.firstname} ${frame.employeeId.lastname}`}
+							</option>;
+						})}
 					</select>
 					<button type="submit">Submit</button>
 				</form>
-			</div>);
+			</div>
+		);
 	}
 }
 
-// const mapStateToProps = state => ({
-// 	isLoading: state.employee.reassign === null,
-// });
+const mapStateToProps = state => ({
+	// isLoading: state.employee.reassign === null,
+	frames: state.frames
+});
 
-export default connect()(ReassignShiftForm);
+export default connect(mapStateToProps)(ReassignShiftForm);
