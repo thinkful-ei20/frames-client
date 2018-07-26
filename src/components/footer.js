4217ccbbb07a875654c58dc3ad3be113181131f9
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-26 12:01:26 -0600

    Add alt text to footer github icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,22 +8,34 @@ export default function Footer() {
 			<ul>
 				<li>
 					<a href="https://github.com/mdargitz">Marina Dargitz</a>
-					<img src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png' />
+					<img
+						src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png'
+						alt='Marina Dargitz Github'
+					/>
 				</li>
 
 				<li>
 					<a href="https://github.com/csprier">Cameron Prier</a>
-					<img src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png' />
+					<img
+						src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png'
+						alt='Cameron Prier Github'
+					/>
 				</li>
 
 				<li>
 					<a href="https://github.com/patersog">Gianluca Paterson</a>
-					<img src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png' />
+					<img
+						src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png'
+						alt='Gianluca Paterson'
+					/>
 				</li>
 
 				<li>
 					<a href="https://github.com/Sakela17">Marina Valiquette</a>
-					<img src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png' />
+					<img
+						src='https://cdn.iconscout.com/public/images/icon/free/png-128/github-logo-brand-development-tools-318f14f5797bb6cc-128x128.png'
+						alt='Marina Valiquette Github'
+					/>
 				</li>
 			</ul>
 		</footer>
