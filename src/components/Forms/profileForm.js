d60d6ddd67cde585d787ac5da4deee6248337d71
patersog <gianluca.paterson@gmail.com>
2018-07-26 15:45:49 -0400

    Fix server-side error handeling in profileForm component

--- src/components/Forms/profileForm.js
+++ src/components/Forms/profileForm.js
@@ -7,6 +7,14 @@ import { editProfile } from '../../actions/profile';
 import PropTypes from 'prop-types';
 
 export class ProfileForm extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			error: null
+		}
+	}
+
 	onSubmit = values => {
 		const updatedProfile = {};
 		Object.keys(values).forEach(key => {
@@ -16,15 +24,32 @@ export class ProfileForm extends React.Component {
 			}
 		});
 		this.props.dispatch(editProfile(this.props.initialValues.adminId, updatedProfile))
-			.then(this.props.setEdit);
+		/* work around for messy state logic with redux-forms, definitely should make it more succinct */
+			.then( res => {
+				if( res ) {
+					this.setState({error: res.error});
+				} else {
+					this.setState({error: null});
+					this.props.setEdit();
+				}
+			});
 	};
 
 	render() {
+		let {error} = this.state;
+		if (error) {
+			error = (
+				<div className="form-error" aria-live="polite">
+					{this.state.error}
+				</div>
+			);
+		}
 		return (
       <div className="form-wrapper">
 				<form
 					onSubmit={this.props.handleSubmit(this.onSubmit)}
-				>
+				>	
+					{error}
 					<Field
 						name="companyName"
 						label="Company Name"
