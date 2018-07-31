eee8b8d6716ea133067c850380a8933b7945877b
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-31 13:04:09 -0600

    Rework landing page verbage to be more client oriented

--- src/components/about.js
+++ src/components/about.js
@@ -14,28 +14,27 @@ export const About = () => {
 				</div>
 				<div className="about-section-text-wrapper">
 					<h3>Mobile Optimized</h3>
-					<p>Interface is optimized to display a large amount of densely organized information on mobile devices.</p>
+					<p>View, update and edit your team's entire schedule on the go in a beautiful, built-for-mobile dashboard.</p>
 				</div>
 			</section>
 
 			<section className="about-section">
 				<div className="about-section-text-wrapper">
 					<h3>Customizable view</h3>
-					<p>Advanced Filter options allow to customize the schedule overview by selecting shift times,
-            or employee name.</p>
+					<p>Filter your schedule however you like with advanced tools that make at-a-glace decsions easier than ever.</p>
 				</div>
 				<div className="about-section-img-wrapper">
-          <img src={dashboardScreenshot} alt="Dashboard screenshot"/>
+					<img src={dashboardScreenshot} alt="Dashboard screenshot"/>
 				</div>
 			</section>
 
 			<section className="about-section">
 				<div className="about-section-img-wrapper">
-          <img src={employeeScreenshot} alt="Employee screenshot"/>
+					<img src={employeeScreenshot} alt="Employee screenshot"/>
 				</div>
 				<div className="about-section-text-wrapper">
 					<h3>Employee management</h3>
-					<p>And it is also a very good one</p>
+					<p>Keep track of all your team members, their contact infromation and their schedule for easy communication and smoother operations.</p>
 				</div>
 			</section>
 
