b4b5ad6e94cb18e4d206c697d200442c8536f03b
patersog <gianluca.paterson@gmail.com>
2018-08-02 11:45:34 -0400

    Update create-employee/frame-form.test.js & Fix #id typo for lastname input in create-employee-form.js

--- src/__tests__/create-employee-form.test.js
+++ src/__tests__/create-employee-form.test.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+
 import {CreateEmployeeForm} from '../components/forms/create-employee-form';
+import {hideModal} from '../actions/modals';
+import {clearFrameError} from '../actions/frames';
 
 describe('Create Employee Form', () => {
+	let wrapper;
+	const dispatch = jest.fn();
+	const props = {
+		error:  null
+	};
 	it('should render without crashing', () => {
-		shallow(<CreateEmployeeForm />);
+		shallow(<CreateEmployeeForm dispatch={dispatch} {...props}/>);
 	});
+
+
+	it('should contain the correct input fields and buttons', () => {
+		wrapper = shallow(<CreateEmployeeForm dispatch={dispatch} {...props}/>);
+		expect(wrapper.find('input #firstname')).toHaveLength(1);
+		expect(wrapper.find('input #lastname')).toHaveLength(1);
+		expect(wrapper.find('input #image')).toHaveLength(1);
+		expect(wrapper.find('input #email')).toHaveLength(1);
+		expect(wrapper.find('input #phoneNumber')).toHaveLength(1);
+		expect(wrapper.find('input #password')).toHaveLength(1);
+		expect(wrapper.find('.form-submit-btn')).toHaveLength(1);
+		expect(wrapper.find('.form-reset-btn')).toHaveLength(1);
+	});
+
+	it('should submit on click', () => {
+		wrapper = shallow(<CreateEmployeeForm dispatch={dispatch} {...props}/>);
+		const preventDefault = jest.fn();
+
+		wrapper.find('form').simulate('submit', {preventDefault});
+		expect(dispatch).toHaveBeenCalled();  // <-- Could be Improved
+	});
+
+	it('should cancle on click', () => {
+		wrapper = shallow(<CreateEmployeeForm dispatch={dispatch} {...props}/>);
+
+		wrapper.find('.form-reset-btn').simulate('click');
+		expect(dispatch).toHaveBeenCalledWith(clearFrameError());
+		expect(dispatch).toHaveBeenCalledWith(hideModal());
+	});
+
 });
--- src/__tests__/create-frame-form.test.js
+++ src/__tests__/create-frame-form.test.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import {CreateShiftForm} from '../components/forms/create-shift-form';
 import {shallow} from 'enzyme';
 
+import {CreateFrameForm} from '../components/forms/create-frame-form';
+import {hideModal} from '../actions/modals';
+
 describe('Create Shift Form Component', () => {
+
+	let wrapper;
+
+	const props = {
+		show: false,
+		error: null,
+		employees: [{}]
+	};
+	const dispatch = jest.fn();
+
 	it('should render without crashing', () => {
 		shallow(
-			<CreateShiftForm
-				show={false}
-				dispatch={jest.fn}
-				error={null}
-				employees={[
-					{}
-				]}
-			/>);
+			<CreateFrameForm dispatch={dispatch} {...props}/>);
+	});
+
+	it('should contain the correct input fields and buttons', () => {
+		wrapper = shallow(<CreateFrameForm dispatch={dispatch} {...props}/>);
+		expect(wrapper.find('select #employee-select')).toHaveLength(1);
+		expect(wrapper.find('input #startDate')).toHaveLength(1);
+		expect(wrapper.find('input #endDate')).toHaveLength(1);
+		expect(wrapper.find('.form-submit-btn')).toHaveLength(1);
+		expect(wrapper.find('.form-reset-btn')).toHaveLength(1);
+	});
+
+	it('should submit on click', () => {
+		wrapper = shallow(<CreateFrameForm dispatch={dispatch} {...props}/>);
+		const preventDefault = jest.fn();
+		wrapper.find('form').simulate('submit', {preventDefault});
+		expect(preventDefault).toHaveBeenCalled();
+		expect(dispatch).toHaveBeenCalled();  // <-- Could be Improved
+	});
+
+	it('should cancle on click', () => {
+		wrapper = shallow(<CreateFrameForm dispatch={dispatch} {...props}/>);
+
+		wrapper.find('.form-reset-btn').simulate('click');
+		expect(dispatch).toHaveBeenCalledWith(hideModal());
 	});
 });
